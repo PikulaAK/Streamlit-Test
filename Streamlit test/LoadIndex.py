7b5890828724267ae32689a9879a39a34dcc372f
PikulaAK <kusleavash@gmail.com>
2024-07-31 15:20:28 -0500

    Added base code

---
+++ Streamlit test/LoadIndex.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import os
+import pickle
+import asyncio
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.llms.ollama import Ollama
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+
+def initialize_chat():
+    print('start initial')
+    # ollama
+    Settings.llm = Ollama(
+        model="llama3.1", 
+        base_url="http://localhost:11434",
+        request_timeout=360.0)
+    
+def Index():
+    with st.sidebar:
+        with st.form("Index_Form"):
+            directory = st.file_uploader("Choose index file", type="pkl")
+            submitted = st.form_submit_button("Load")
+            if submitted:  # Check if a file was uploaded
+                file_details = {"FileName":directory.name,"FileType":directory.type}
+                st.write(file_details)
+                save_uploadedfile(directory)
+                st.write("Loading index file...")
+                file_path = r"tempDir\\" + directory.name
+                index = load_index_from_file(file_path)
+                st.write("Loaded index file")
+                os.remove(r"tempDir\\"+directory.name)
+                st.session_state.chat_engine_mem = index.as_chat_engine(
+                chat_mode="condense_plus_context" #, streaming=True, 
+                )
+        initialize_chat() 
+        #index = VectorStoreIndex.from_documents(documents,)    
+        #print (documents)
+        prompt = ""
+
+def load_index_from_file(file_path):
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
+    
+####Left Side Bar###
+with st.sidebar:
+    with st.expander("Prompts"):
+        qa_prompt_template = st.text_area("QA Prompt", height=300,  key="qa_prompt_template",value="")    
+
+def reset_conversation():
+    st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state.chat_history = '[]'
+    st.session_state.chat_engine_mem.reset()
+
+async def chat_stream_api():
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])  
+    if user_query := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": user_query})
+        st.chat_message("user").write(user_query)
+        with st.spinner("preparing your information."):
+            # Display chatbot response in Streamlit chat UI
+            with st.chat_message('assistant'):
+                # Get chatbot response
+                streaming_response = st.session_state.chat_engine_mem.stream_chat(user_query)
+                st.write_stream(streaming_response.response_gen)
+                st.session_state.messages.append({"role": "assistant", "content": streaming_response.response})
+            
+    st.button('New Conversation', on_click=reset_conversation)            
+st.empty()    
+st.caption("🚀 To infinity and beyond.")
+
+if __name__ == "__main__":  
+    if "chat_engine_mem" not in st.session_state: 
+        print ("doc not in")
+        initialize_chat()
+    
+    asyncio.run (chat_stream_api())
+
+    
+Index()
---
+++ Streamlit test/SaveIndex.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import os
+import pickle
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+
+
+def save_index_to_file(index, file_path):
+    with open(file_path, 'wb') as f:
+        pickle.dump(index, f)
+        
+
+def Document():
+    with st.form("Document_Form"):
+        DocumentDirectory = st.file_uploader("Choose document(s)",type=['pdf','txt'])
+        IndexDirectory = st.text_input("Directory to save index file")
+        submitted = st.form_submit_button("Index")
+        if submitted:
+            file_details = {"FileName":DocumentDirectory.name,"FileType":DocumentDirectory.type}
+            st.write(file_details)
+            save_uploadedfile(DocumentDirectory)
+            st.write("Reading Documents...")
+            documents = SimpleDirectoryReader("tempDir").load_data()
+            #documents
+
+            index_file_path = IndexDirectory + r"\\index.pkl"
+
+            Settings.embed_model = OllamaEmbedding(
+                model_name="nomic-embed-text",
+                base_url="http://localhost:11434",
+                ollama_additional_kwargs={"mirostat": 0},
+            )
+
+            #vectorize the documents
+            st.write("Vectorizing Documents...")
+            index = VectorStoreIndex.from_documents(
+                documents,
+            )
+
+            # Save the index to a file
+            st.write("Saving index file...\n")
+            save_index_to_file(index, index_file_path)
+            st.write("Saved index file successfuly")
+            os.remove("tempDir\\"+DocumentDirectory.name)
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+
+
+
+
+# DocOrIndex()
+Document()
